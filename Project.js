39c5b79a2ed5adaad8294726963bd6783d9d55f4
Chikodiri <ibeakanmachikodiri@gmail.com>
2024-03-11 23:01:22 -0400

    adding a close modal function

--- Project.js
+++ Project.js
@@ -16,3 +16,8 @@ function modal(modalId) {
     const modal = document.getElementById(modalId);
     modal.classList.add("open");
 }
+
+function closeModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.classList.remove("open");
+}
