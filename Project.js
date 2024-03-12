21eeaadf29972fcb4aec2b7dc5926130b0ef0f68
Chikodiri <ibeakanmachikodiri@gmail.com>
2024-03-11 23:10:50 -0400

    Standardize comment style in the js

--- Project.js
+++ Project.js
@@ -2,9 +2,7 @@ const outerGrid = document.querySelector(".outer-grid");
 const navShow = document.querySelector(".navbar");
 const main = document.querySelector(".main");
 
-/* 
-1 Mobile menu toggle button
-*/
+// Mobile menu toggle button
 const bars = document.getElementById("bars");
 bars.addEventListener('click', () => {
     navShow.classList.toggle("show");
