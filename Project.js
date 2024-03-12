997ddde7eaacfe3c39df96a856115e59f695519c
Chikodiri <ibeakanmachikodiri@gmail.com>
2024-03-11 22:54:26 -0400

    adding comment

--- Project.js
+++ Project.js
@@ -2,6 +2,9 @@ const outerGrid = document.querySelector(".outer-grid");
 const navShow = document.querySelector(".navbar");
 const main = document.querySelector(".main");
 
+/* 
+1 Mobile menu toggle button
+*/
 const bars = document.getElementById("bars");
 bars.addEventListener('click', () => {
     navShow.classList.toggle("show");
