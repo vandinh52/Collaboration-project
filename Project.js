44f440696923051c77bb202ee5c0ad78c0c6e534
Chikodiri <ibeakanmachikodiri@gmail.com>
2024-03-11 22:53:14 -0400

    adding menu button

--- Project.js
+++ Project.js
@@ -1,3 +1,9 @@
 const outerGrid = document.querySelector(".outer-grid");
 const navShow = document.querySelector(".navbar");
 const main = document.querySelector(".main");
+
+const bars = document.getElementById("bars");
+bars.addEventListener('click', () => {
+    navShow.classList.toggle("show");
+    outerGrid.classList.toggle("outer-grid-expanded");
+})
