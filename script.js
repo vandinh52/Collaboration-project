e32a83ee5948c05212a2cf22cf8fa0a11985a590
Emmanuel Diogu <emmanueldiogu@gmail.com>
2024-03-14 17:08:30 -0400

    changed format

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ const main = document.querySelector(".main");
 
 // Mobile menu toggle button
 const bars = document.getElementById("bars");
-bars.addEventListener('click', () => {
+bars.addEventListener("click", () => {
     navShow.classList.toggle("show");
     outerGrid.classList.toggle("outer-grid-expanded");
-})
+});
 
 // Modal function open
 function openModal(modalId) {
@@ -24,15 +24,14 @@ function closeModal(modalId) {
 the count on the download on click button increased
 */
 // Get the fa-download icon element
-const downloadIcon = document.querySelector('.fa-download');
+const downloadIcon = document.querySelector(".fa-download");
 // Get the count element
-const downloadCount = document.querySelector('#downloadCount');
+const downloadCount = document.querySelector("#downloadCount");
 // Set the initial count
 let count = 105;
-downloadIcon.addEventListener('click', function () {
+downloadIcon.addEventListener("click", function () {
     // Increase the count
     count += 1;
     // Update the count in the DOM
     downloadCount.textContent = count;
 });
-
